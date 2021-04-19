a1c12c1d523ba701611dc056acf1eb50e59fa6e2
imo999 <rcajina.pro@gmail.com>
2021-04-19 22:07:34 +0200

    update : contact TODOO

---
+++ src/components/ContactInfo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ContactInfo = () => {
+  return (
+    <div>
+      <div className="contact-infos">
+        <div className="address">
+          <div className="content">
+            <h4>adresse</h4>
+            <p>3 Rue des Huguenots</p>
+            <p>89330 - Saint Julien du Sault</p>
+          </div>
+        </div>
+      </div>
+      <div className="phone">
+        <div className="content">
+          <h4>téléphone</h4>
+          <p>06-45-25-30-49</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactInfo
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
 import ButtonsBottom from '../components/ButtonsBottom'
+import ContactInfo from '../components/ContactInfo'
 
 const Contact = () => {
   return (
     <div>
       <Navigation />
       <div>
-        Contact
+        <ContactInfo />
       </div>
       <ButtonsBottom left="/project-5" />
     </div>
