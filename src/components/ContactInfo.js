d6e657c4fd68c492571d5a249494c9a43bd64119
imo999 <rcajina.pro@gmail.com>
2021-04-19 22:39:34 +0200

    update : contact TODOO

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,22 +1,52 @@
 import React from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import SocialNetwork from './SocialNetwork';
 
 const ContactInfo = () => {
   return (
-    <div>
-      <div className="contact-infos">
-        <div className="address">
-          <div className="content">
-            <h4>adresse</h4>
-            <p>3 Rue des Huguenots</p>
-            <p>89330 - Saint Julien du Sault</p>
-          </div>
+    <div className="contact-infos">
+      <div className="address">
+        <div className="content">
+          <h4>adresse</h4>
+          <p>3 rue des Hugeunots</p>
+          <p>89330 - St Julien du Sault</p>
         </div>
       </div>
       <div className="phone">
         <div className="content">
-          <h4>téléphone</h4>
-          <p>06-45-25-30-49</p>
+          <h4>Phone</h4>
+          <CopyToClipboard text="33645253049" className="hover">
+            <p
+              style={{ cursor: 'pointer' }}
+              className="clipboard"
+              onClick={() => {
+                alert("Phone copied !");
+              }}
+            >
+              +33 06 45 25 30 49
+                </p>
+          </CopyToClipboard>
         </div>
+      </div>
+      <div className="email">
+        <div className="content">
+          <h4>email</h4>
+          <CopyToClipboard text="contact@imo999.io" className="hover">
+            <p
+              style={{ cursor: 'pointer' }}
+              className="clipboard"
+              onClick={() => {
+                alert("Email copied !");
+              }}
+            >
+              contact@imo999.io
+                </p>
+          </CopyToClipboard>
+        </div>
+      </div>
+      <SocialNetwork />
+      <div className="credits">
+        <p>By Imo999 - 2021</p>
       </div>
     </div>
   )
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,17 +2,49 @@ import React from 'react'
 import Navigation from '../components/Navigation'
 import ButtonsBottom from '../components/ButtonsBottom'
 import ContactInfo from '../components/ContactInfo'
+import { motion } from "framer-motion";
 
 const Contact = () => {
+
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 300,
+    },
+  };
+
+  const transition = {
+    ease: [.03, .87, .73, .9],
+    duration: .6,
+  }
+
+
+
   return (
-    <div>
-      <Navigation />
-      <div>
-        <ContactInfo />
-      </div>
-      <ButtonsBottom left="/project-5" />
-    </div>
+    <main>
+      <motion.div
+        className="contact"
+        exit="out"
+        animate="in"
+        initial="out"
+        variants={pageTransition}
+        transition={transition}
+      >
+        <div>
+          <Navigation />
+          <div>
+            <ContactInfo />
+          </div>
+          <ButtonsBottom left="/project-5" />
+        </div>
+      </motion.div>
+    </main>
   )
+
 }
 
 export default Contact
